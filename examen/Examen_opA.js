cd081af90ae4ade532de28836bc6680d3788ff0b
Elbachir <elbachirzriguinat@gmail.com>
2024-11-24 23:28:48 +0100

    opcions

--- examen/Examen_opA.js
+++ examen/Examen_opA.js
@@ -1,18 +1,18 @@
 
 
 function buscarPalabras() {
-
+    //aqui declaracion del texto insertado en HTML y el caracter en el HTML
     const texto = document.getElementById("text").value.trim();
     const caracter = document.getElementById("character").value.trim();
-    const mensaje = document.getElementById("message");
-    const listaResultados = document.getElementById("lista_resultat");
+    const mensaje = document.getElementById("message"); //declarar un mensaje
+    const listaResultados = document.getElementById("lista_resultat"); //listar los  resultados
     
     
     mensaje.innerText = "LAS PALABRAS Y LAS POSICIONES !!";
-    listaResultados.innerHTML = "";
+    listaResultados.innerHTML ="";
     
     
-    const palabras = texto.toLowerCase().split(" ");
+    const palabras = texto.toLowerCase().split(" ");   //convertir los carateres en minisculas
     if (palabras.length < 2) {
         
         alert("el texto debe contener como minimo 2 palabras");
@@ -30,14 +30,16 @@ function buscarPalabras() {
             const item = document.createElement("li");
             const palabraResaltada = palabra.slice(0, posicion) + "<b>" + palabra.charAt(posicion) + "</b>" + palabra.slice(posicion + 1);
                
-        
+         
+
             item.innerHTML = `Palabra: ${palabraResaltada} - Posición: ${posicion + 1}`;
             listaResultados.appendChild(item);
-        
+            
         }
     });
 
     if (palabrasEncontradas === 0) {
+        
         alert("no se encontro ninguna palabra que contenga el caracter insertado !!");
         mensaje.innerText="INTENTALO OTRA VEZ !!";
     }
